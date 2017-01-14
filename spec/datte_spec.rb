c6dd9dd8e0a45204754e92b551192f50ae39c6f9
pokohide <hyde31415@gmail.com>
2017-01-14 19:49:22 +0900

    テスト追加

--- spec/datte_spec.rb
+++ spec/datte_spec.rb
@@ -5,7 +5,13 @@ describe Datte do
     expect(Datte::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "::Parser" do
+    datte = Datte::Parser.new
+
+    it "2016/11/1日に遊ぼー" do
+      body = "2016/11/1日に遊ぼー"
+      d = DateTime.now
+      expect(datte.parse_date(body)).to eq(DateTime.new(2016, 11, 1, d.hour, 0, 0))
+    end
   end
 end
